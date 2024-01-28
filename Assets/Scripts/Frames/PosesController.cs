f3143d8086e2cdc6dbe23dd2a96dbf5f0dd74f4b
Carlos <carlos.enriquez.valerio@gmail.com>
2024-01-28 12:58:55 -0600

    Timer

--- Assets/Scripts/Frames/PosesController.cs
+++ Assets/Scripts/Frames/PosesController.cs
@@ -8,27 +8,35 @@ public class PosesController : MonoBehaviour
     [SerializeField] private List<Sprite> _poseList;
     [SerializeField] private float _timeToStart;
     [SerializeField] private float _timeToSpawn;
+    //[SerializeField] private float _timeToEvaluate=;
     [SerializeField] private Transform _poseTransfrom;
     private GameObject _currentPose;
     public bool DoPoses { get; private set; }
-
+    public float CurrentTime { get; private set; }
     private IEnumerator Start()
     {
-        DoPoses=true;
+        CurrentTime = _timeToSpawn;
         _currentPose = Instantiate(_posePrefab, _poseTransfrom.position, Quaternion.identity);
         _currentPose.SetActive(false);
         yield return new WaitForSeconds(_timeToStart);
-
+        DoPoses = true;
+        CreatePose();
         while (DoPoses)
         {
-            CreatePose();
-            yield return new WaitForSeconds(_timeToSpawn);
+            CurrentTime -= Time.deltaTime;
+            if (!( Mathf.CeilToInt(CurrentTime) < 0))
+                yield return null;
+            else
+            {
+                CurrentTime = _timeToSpawn;
+                CreatePose();
+            }
         }
     }
 
     private void CreatePose()
     {
-       _currentPose.GetComponent<SpriteRenderer>().sprite = _poseList[Random.Range(0, _poseList.Count)];
+        _currentPose.GetComponent<SpriteRenderer>().sprite = _poseList[Random.Range(0, _poseList.Count)];
         _currentPose.SetActive(true);
     }
 }
--- Assets/Scripts/UI/UIPoseTimer.cs
+++ Assets/Scripts/UI/UIPoseTimer.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.InputSystem.XR;
+
 
 public class UIPoseTimer : MonoBehaviour
 {
-//   [SerializeField] private void TM
+    [SerializeField] private TMPro.TextMeshProUGUI _text;
+    private PosesController _poses;
+    private void Awake()
+    {
+        _poses = FindObjectOfType<PosesController>();
+    }
+    private void Update()
+    {
+        _text.text = Mathf.CeilToInt(_poses.CurrentTime).ToString();
+    }
 }
